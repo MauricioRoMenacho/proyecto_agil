8656b135d4229c2b672a271d627ccc97917ce5b0
MauricioRoMenacho <mauricio.ro1@hotmail.com>
2026-05-20 20:48:40 -0500

    conectado con node

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", "application/json");
+    if (req.url === "/user") {
+        const user = {
+            id: 1,
+            name: "Mauricio",
+            email: "[EMAIL_ADDRESS]"
+        };
+        res.end(JSON.stringify(user));
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
